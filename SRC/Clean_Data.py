f6b4e851453ebec06790099c895d0d1e76d26c1f
Zoe <zoetreutens@gmail.com>
2024-05-11 10:50:03 +0200

    Cleaned Data Completed

--- SRC/Clean_Data.py
+++ SRC/Clean_Data.py
@@ -90,18 +90,18 @@ credit_risk_raw_data_copy['loan_int_rate'].fillna(credit_risk_raw_data_copy['loa
 print(f"Number of Missing Values in credit_risk_raw_data_copy:\n{credit_risk_raw_data_copy.isnull().sum()}\n")
 
 """Answer: 
-Number of Missing Values in credit_risk_raw_data_copy:       
-    person_age                 0
-    person_income              0
-    person_home_ownership      0
-    person_emp_length          0
-    loan_intent                0
-    loan_amnt                  0
-    loan_int_rate              0
-    loan_percent_income        0
-    person_cred_hist_length    0
-    loan_status                0
-    dtype: int64
+    - Number of Missing Values in credit_risk_raw_data_copy:       
+        person_age                 0
+        person_income              0
+        person_home_ownership      0
+        person_emp_length          0
+        loan_intent                0
+        loan_amnt                  0
+        loan_int_rate              0
+        loan_percent_income        0
+        person_cred_hist_length    0
+        loan_status                0
+        dtype: int64
 """
 
 # 3. For both dataset, check for duplicate records and remove them from the dataset
@@ -109,21 +109,56 @@ print(f"Number of duplicate rows in raw_data_copy: {credit_risk_raw_data_copy.du
 print(f"Number of duplicate rows in validation_data_copy: {credit_risk_validation_data_copy.duplicated().sum()}\n")
 
 """ Answers:
-Number of duplicate rows in raw_data_copy: 1
-Number of duplicate rows in validation_data_copy: 0
+    - Number of duplicate rows in raw_data_copy: 1
+    - Number of duplicate rows in validation_data_copy: 0
 """
 
 # There is a duplicate record in the credit_risk_raw_data_copy dataset, as a result it will need to be dropped 
 credit_risk_raw_data_copy = credit_risk_raw_data_copy.drop_duplicates()
 
-#Check that the duplicate record was remove t
+#Check that the duplicate record was removed
 print(f"Number of duplicate rows in raw_data_copy: {credit_risk_raw_data_copy.duplicated().sum()}\n")
+""" Answers: Number of duplicate rows in raw_data_copy: 0 """
 
-""" Answers:
-Number of duplicate rows in raw_data_copy: 0
-"""
-
-#Convert the datatype of the attribute 'person_emp_length' in credit_risk_validation_data_copy to int64
+# 4. Convert the datatype of the attribute 'person_emp_length' in credit_risk_validation_data_copy to int64
 credit_risk_validation_data_copy['person_emp_length'] = credit_risk_validation_data_copy['person_emp_length'].astype('int64')
 print(f"person_emp_length datatype: {credit_risk_validation_data_copy['person_emp_length'].dtypes}\n")
 """Answer: person_emp_length datatype: int64"""
+
+# 4. For both dataset, perform outliers Data Handling using log transformation
+#person_income
+# For the 'credit_risk_validation_data_copy' dataset
+credit_risk_validation_data_copy = credit_risk_validation_data_copy[credit_risk_validation_data_copy['person_income'] < 1000000]
+credit_risk_raw_data_copy = credit_risk_raw_data_copy[credit_risk_raw_data_copy['person_income'] < 1000000]
+
+print(f"credit_risk_raw_data_copy person_emp_length datatype:\n{credit_risk_raw_data_copy['person_income'].describe()}\n")
+print(f"credit_risk_validation_data_copy person_emp_length datatype:\n{credit_risk_validation_data_copy['person_income'].describe()}\n")
+
+"""
+Answer:
+    - credit_risk_raw_data_copy person_emp_length datatype: 
+        count      1523.000000
+        mean      60931.252791
+        std       66339.170965
+        min        4000.000000
+        25%       34000.000000
+        50%       42000.000000
+        75%       60758.000000
+        max      900000.000000
+        Name: person_income, dtype: float64
+
+    - credit_risk_validation_data_copy person_emp_length datatype: 
+        count       469.000000
+        mean      68590.279318
+        std       69577.787894
+        min        9600.000000
+        25%       34000.000000
+        50%       55000.000000
+        75%       77052.000000
+        max      762000.000000
+        Name: person_income, dtype: float64
+"""
+
+# 5. For both datasets, write the new datasets to CSV files
+credit_risk_raw_data_copy.to_csv('Data/Clean Data/cleaned_credit_risk_raw_data_copy.csv', index=False)
+credit_risk_validation_data_copy.to_csv('Data/Clean Data/cleaned_credit_risk_validation_data.csv', index=False)
